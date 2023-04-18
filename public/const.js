e90338adc7084a205fd8f7c71719dc3fb4f5f97b
Randy <randy.chong@siemens.com>
2023-04-18 14:36:55 +0800

    deployment

---
+++ public/const.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA4afW-D3QA7iSI_hHSGwyWcd4l0M2DkcM",
+    authDomain: "tower-of-hanoi-666.firebaseapp.com",
+    projectId: "tower-of-hanoi-666",
+    storageBucket: "tower-of-hanoi-666.appspot.com",
+    messagingSenderId: "675151473215",
+    appId: "1:675151473215:web:e9caee273239437e82268a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
