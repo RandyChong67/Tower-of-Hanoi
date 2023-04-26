09ea985725cf475fa478e9d80c0fb63d6fa2c9b7
Randy <randy.chong@siemens.com>
2023-04-26 17:20:10 +0800

    minor commit

--- private/gulpfile.js
+++ private/gulpfile.js
@@ -1,15 +1,12 @@
 var gulp = require('gulp'),
-    gulp_less = require('gulp-less'),
-    gulp_auto_prefixer = require('gulp-autoprefixer');
+    gulp_less = require('gulp-less');
 
 //Creating a Style task that convert LESS to CSS
-
 gulp.task('styles', function () {
-    var src_file = 'style.less',
+    var src_file = '../public/style.less',
         temp = './style';
     return gulp
         .src(src_file)
         .pipe(gulp_less())
-        .pipe(gulp_auto_prefixer({browsers: ['last 2 versions', '>5%']}))
         .pipe(gulp.dest(temp));
 });
